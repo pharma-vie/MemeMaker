47ff88e3c669a15bf4171cb576175fef2e6b35c3
dinosilvestro <dino@dinosilvestro.com>
2016-11-28 13:25:54 -0500

    feat: Initial commit

---
+++ app/src/main/java/com/dinosilvestro/mememaker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dinosilvestro.mememaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
