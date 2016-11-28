15ce60273f80d3f33af35b0fadb2f1cb68b8b38f
dinosilvestro <dino@dinosilvestro.com>
2016-11-28 14:34:38 -0500

    feat: Add sign in flow with Firebase UI

--- app/src/main/java/com/dinosilvestro/mememaker/MainActivity.java
+++ app/src/main/java/com/dinosilvestro/mememaker/MainActivity.java
@@ -1,13 +1,63 @@
 package com.dinosilvestro.mememaker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            // user is signed in!
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            return;
+        }
+
+        // Sign in canceled
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, R.string.sign_in_cancelled_text, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // No network
+        if (resultCode == ResultCodes.RESULT_NO_NETWORK) {
+            Toast.makeText(this, R.string.no_internet_connection_text, Toast.LENGTH_SHORT).show();
+
+        }
+
+        // User is not signed in. Maybe just wait for the user to press
+        // "sign in" again, or show a message.
     }
 }
