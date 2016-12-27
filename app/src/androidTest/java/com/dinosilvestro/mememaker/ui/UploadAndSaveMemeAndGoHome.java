ce4c67499b8d2457fdb404e1d44d5de8a24571ad
dinosilvestro <dino@dinosilvestro.com>
2016-12-27 15:15:09 -0500

    test: Add test to check if uploading a custom image works as intended

---
+++ app/src/androidTest/java/com/dinosilvestro/mememaker/ui/UploadAndSaveMemeAndGoHome.java
@@ -0,0 +1,130 @@
+package com.dinosilvestro.mememaker.ui;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.dinosilvestro.mememaker.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class UploadAndSaveMemeAndGoHome {
+
+    @Rule
+    public ActivityTestRule<SplashScreenActivity> mActivityTestRule = new ActivityTestRule<>(SplashScreenActivity.class);
+
+    @Test
+    public void uploadAndSaveMemeAndGoHome() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3598912);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.create_meme_action_button),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3594242);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction floatingActionButton2 = onView(
+                allOf(withId(R.id.upload_meme_action_button),
+                        withParent(allOf(withId(R.id.activity_meme_display),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        floatingActionButton2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3594310);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.top_edit_text),
+                        withParent(allOf(withId(R.id.activity_meme_edit),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.top_edit_text),
+                        withParent(allOf(withId(R.id.activity_meme_edit),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.top_edit_text),
+                        withParent(allOf(withId(R.id.activity_meme_edit),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("test"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.bottom_edit_text),
+                        withParent(allOf(withId(R.id.activity_meme_edit),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("test"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction floatingActionButton3 = onView(
+                allOf(withId(R.id.save_meme_action_button),
+                        withParent(allOf(withId(R.id.activity_meme_edit),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        floatingActionButton3.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open"),
+                        withParent(allOf(withId(R.id.toolbar),
+                                withParent(withId(R.id.activity_meme_edit)))),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.material_drawer_recycler_view),
+                        withParent(allOf(withId(R.id.material_drawer_slider_layout),
+                                withParent(withId(R.id.material_drawer_layout)))),
+                        isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
+
+    }
+
+}
